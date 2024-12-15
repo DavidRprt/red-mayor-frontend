33220450cd2dcbe1e16669d65494cd233b9a12e0
DavidRprt <davirapo@gmail.com>
2024-12-15 14:06:35 -0300

    Redirecting to /admin

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next"
+
 const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || ""
 const apiHost = new URL(apiBaseUrl).hostname
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [apiHost], 
+    domains: [apiHost], // Permite cargar imágenes desde el dominio del backend
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/admin/:path*",
+        destination: `${apiBaseUrl}/admin/:path*`, // Redirige a la ruta de admin del backend
+      },
+    ]
   },
 }
 
