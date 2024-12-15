dfffaed82e2e822fe1f33fb705fdd627bcb2b382
DavidRprt <davirapo@gmail.com>
2024-12-15 15:57:31 -0300

    Fixing image bug

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next"
 
 const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || ""
-const apiHost = new URL(apiBaseUrl).hostname
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: [apiHost], // Permite cargar imágenes desde el dominio del backend
-  },
   async rewrites() {
     return [
       {
